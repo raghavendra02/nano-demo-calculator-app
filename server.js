fe38d9b444196d593a50a2751521b127f4dbb61a
Raghavendra A K <raghavendra2002ak@gmail.com>
2023-08-24 12:34:45 +0530

    added add and sub for server file

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const log = require('ipfs-api/src/log');
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -10,18 +11,22 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('Hello World!');
+    return res.send('Hello Calculator');
 });
 
 baseRouter.post('/add', (req, res) => {
-    const { first, second } = req.body;
-    res.json({ "result": first + second });
+    const first = Number(req.body.first);
+    const second = Number(req.body.second);
+    const result = first + second;
+    res.json({ "result": result });
 });
 
 
 baseRouter.post('/subtract', (req, res) => {
-    const { first, second } = req.body;
-    res.json({ "result": first - second });
+    const first = Number(req.body.first);
+    const second = Number(req.body.second);
+    const result = first - second;
+    res.json({ "result": result });
 });
 
 app.use(baseUrl, baseRouter);
